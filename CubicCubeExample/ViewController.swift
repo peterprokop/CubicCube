cd5abfcaa6a8ad2eabb6b2998a1d86f01f1ee8e7
Rick Sanchez <peter.prokop@blackswan.com>
2018-07-02 21:53:38 +0100

    Initial Commit

---
+++ CubicCubeExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CubicCubeExample
+//
+//  Created by Peter Prokop on 02/07/2018.
+//  Copyright © 2018 Peter Prokop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
